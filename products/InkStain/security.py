88c9493e92a733244e357925f704c92df2f11252
Sebastian Gottfried <sebastiangottfried@web.de>
2010-10-19 20:44:24 +0200

    remove Inkstain guestbook product, it's incopatible to Plone 4.0

--- products/InkStain/security.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from Products.CMFCore.permissions import setDefaultRoles
-
-
-AddISGuestbook = 'InkStain: Add guestbook'
-AddISSignature = 'InkStain: Add signature'
-
-setDefaultRoles(AddISGuestbook, ('Manager', 'Owner',))
-setDefaultRoles(AddISSignature, ('Member', 'Anonymous',))
