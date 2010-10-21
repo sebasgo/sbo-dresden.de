a07ede8c4d1b90b08c97d4830f598f3e11698ad5
Sebastian Gottfried <sebastiangottfried@web.de>
2010-10-21 19:07:10 +0200

    reactivate theme product
    - include in buildout.cfg
    - base skin layer on 'Sunburst' instead of 'Plone Default'
    - introduce new viewlet for two level tab navigation
    - remove old css markup

--- src/sbo.theme/sbo/theme/browser/viewlets.py
+++ src/sbo.theme/sbo/theme/browser/viewlets.py
@@ -1,21 +1,121 @@
+from zope.interface import implements, alsoProvides
+from zope.component import getMultiAdapter
+from zope.viewlet.interfaces import IViewlet
+from zope.deprecation.deprecation import deprecate
+
+from plone.app.layout.globals.interfaces import IViewView
+
+from AccessControl import getSecurityManager
+from Acquisition import aq_base, aq_inner
+from Products.CMFPlone.utils import safe_unicode
+from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone import utils
+from Products.CMFPlone.browser.navigation import get_url,get_id,get_view_url
+from cgi import escape
+from urllib import quote_plus
+from plone.app.layout.navigation.root import getNavigationRoot
 
-# Sample code for a basic viewlet (In order to use it, you'll have to):
-# - Un-comment the following useable piece of code (viewlet python class).
-# - Rename the viewlet template file ('browser/viewlet.pt') and edit the
-#   following python code accordingly.
-# - Edit the class and template to make them suit your needs.
-# - Make sure your viewlet is correctly registered in 'browser/configure.zcml'.
-# - If you need it to appear in a specific order inside its viewlet manager,
-#   edit 'profiles/default/viewlets.xml' accordingly.
-# - Restart Zope.
-# - If you edited any file in 'profiles/default/', reinstall your package.
-# - Once you're happy with your viewlet implementation, remove any related
-#   (unwanted) inline documentation  ;-p
+class ViewletBase(BrowserView):
+    """ Base class with common functions for link viewlets.
+    """
+    implements(IViewlet)
 
-#class MyViewlet(ViewletBase):
-#    render = ViewPageTemplateFile('viewlet.pt')
-#
-#    def update(self):
-#        self.computed_value = 'any output'
+    def __init__(self, context, request, view, manager):
+        super(ViewletBase, self).__init__(context, request)
+        self.__parent__ = view
+        self.context = context
+        self.request = request
+        self.view = view
+        self.manager = manager
+
+    @property
+    @deprecate("Use site_url instead. ViewletBase.portal_url will be removed in Plone 4")
+    def portal_url(self):
+        return self.site_url
+
+
+    def update(self):
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+        self.site_url = self.portal_state.portal_url()
+        self.navigation_root_url = self.portal_state.navigation_root_url()
+
+    def render(self):
+        # defer to index method, because that's what gets overridden by the template ZCML attribute
+        return self.index()
+
+    def index(self):
+        raise NotImplementedError(
+            '`index` method must be implemented by subclass.')
+
+class GlobalNavViewlet(ViewletBase):
+    index = ViewPageTemplateFile('templates/global_nav.pt')
+
+    def update(self):
+        context = aq_inner(self.context)
+        portal_tabs_view = getMultiAdapter((context, self.request),
+                                           name='portal_tabs_view')
+        self.portal_tabs = portal_tabs_view.topLevelTabs()
+        
+        selectedTabs = self.context.restrictedTraverse('selectedTabs')
+        self.selected_tabs = selectedTabs('index_html',
+                                          self.context,
+                                          self.portal_tabs)
+        self.selected_portal_tab = self.selected_tabs['portal']
+
+        for tab in self.portal_tabs:
+            if tab['id']!='Members':
+                tab['subtab']=self.getsubtab(self.context,tab)
+            else:
+                tab['subtab']=[]
+
+    def getsubtab(self,context,tab):
+        query={}
+        result=[]
+        portal_properties = getToolByName(context, 'portal_properties')
+        portal_catalog = getToolByName(context, 'portal_catalog')
+        navtree_properties = getattr(portal_properties, 'navtree_properties')
+        site_properties = getattr(portal_properties, 'site_properties')
+
+        rootPath = getNavigationRoot(context)
+        dpath='/'.join([rootPath,tab['id']])
+        query['path'] = {'query' : dpath, 'depth' : 1}
+
+        query['portal_type'] = utils.typesToList(context)
+
+        sortAttribute = navtree_properties.getProperty('sortAttribute', None)
+        if sortAttribute is not None:
+            query['sort_on'] = sortAttribute
+
+            sortOrder = navtree_properties.getProperty('sortOrder', None)
+            if sortOrder is not None:
+                query['sort_order'] = sortOrder
+
+        if navtree_properties.getProperty('enable_wf_state_filtering', False):
+            query['review_state'] = navtree_properties.getProperty('wf_states_to_show', [])
+
+        query['is_default_page'] = False
+
+        if site_properties.getProperty('disable_nonfolderish_sections', False):
+            query['is_folderish'] = True
+
+        # Get ids not to list and make a dict to make the search fast
+        idsNotToList = navtree_properties.getProperty('idsNotToList', ())
+        excludedIds = {}
+        for id in idsNotToList:
+            excludedIds[id]=1
+
+        rawresult = portal_catalog.searchResults(**query)
+
+        # now add the content to results
+        for item in rawresult:
+            if not (excludedIds.has_key(item.getId) or item.exclude_from_nav):
+                id, item_url = get_view_url(item)
+                data = {'name'      : utils.pretty_title_or_id(context, item),
+                        'id'         : item.getId,
+                        'url'        : item_url,
+                        'description': item.Description}
+                result.append(data)
+        return result
