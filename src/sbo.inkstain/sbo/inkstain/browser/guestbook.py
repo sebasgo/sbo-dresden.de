ddbf2ecb37969688fe23cca2bc05d4142814a9b8
Sebastian Gottfried <sebastiangottfried@web.de>
2010-11-21 12:05:23 +0100

    Add the skeleton for the default guestbook view.

---
+++ src/sbo.inkstain/sbo/inkstain/browser/guestbook.py
@@ -0,0 +1,5 @@
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class GuestbookView(BrowserView):
+    __call__ = ViewPageTemplateFile('guestbook.pt')
