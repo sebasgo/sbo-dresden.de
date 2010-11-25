0770e3143125060d018b0a30881b4f1c17c135b1
Sebastian Gottfried <sebastiangottfried@web.de>
2010-11-25 19:03:13 +0100

    Implement review mode for guestbook.

--- src/sbo.inkstain/sbo/inkstain/browser/guestbook.py
+++ src/sbo.inkstain/sbo/inkstain/browser/guestbook.py
@@ -1,11 +1,43 @@
+from Acquisition import aq_inner
+from zope.security import checkPermission
+
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Acquisition import aq_inner
+from Products.statusmessages.interfaces import IStatusMessage
+
+from sbo.inkstain import InkstainMessageFactory as _
+
 
 class GuestbookView(BrowserView):
-    __call__ = ViewPageTemplateFile('guestbook.pt')
+    template = ViewPageTemplateFile('guestbook.pt')
+
+    def __call__(self):
+        postback = True
+        form = self.request.form
+
+        deleteButton = form.get('form.button.Delete', False)
+
+        if form.get('form.button.Delete', False):
+            deleteIds = form.get('messages', [])
+            for deleteId in deleteIds:
+                for index, entry in enumerate(self.context.entries):
+                    if entry.getId() == deleteId:
+                        del(self.context.entries[index])
+                        break
+            confirm = _(u"Marked messages have been deleted.")
+            IStatusMessage(self.request).addStatusMessage(confirm, type='info')
+            postback = False
+
+        if postback:
+            return self.template()
+        else:
+            self.request.response.redirect(self.context.absolute_url())
+            return ''
 
     def entries(self):
         context = aq_inner(self.context)
         return context.entries[::-1]
+
+    def canReviewGuestbook(self):
+        return checkPermission("sbo.inkstain.ReviewGuestbook", self.context)
 
