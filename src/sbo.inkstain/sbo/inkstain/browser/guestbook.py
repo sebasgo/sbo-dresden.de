d33d052f2e25ce8874a89edae43a9d0de45dae8a
Sebastian Gottfried <sebastiangottfried@web.de>
2010-11-25 16:52:47 +0100

    Implement default view for guestbook archetype.

--- src/sbo.inkstain/sbo/inkstain/browser/guestbook.py
+++ src/sbo.inkstain/sbo/inkstain/browser/guestbook.py
@@ -1,8 +1,11 @@
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Acquisition import aq_inner
 
 class GuestbookView(BrowserView):
     __call__ = ViewPageTemplateFile('guestbook.pt')
-    
-    def canAddSignature(self):
-        return True
+
+    def entries(self):
+        context = aq_inner(self.context)
+        return context.entries[::-1]
+
