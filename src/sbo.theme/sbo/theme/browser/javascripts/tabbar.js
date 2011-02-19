5d8fca4902efd7c6690467b1ae7b07aa59557eae
Sebastian Gottfried <sebastiangottfried@web.de>
2011-02-19 13:47:18 +0100

    animate global navigation
    Current tab highlighting is animated as well as the showing and hiding of the level two tabbars.

--- src/sbo.theme/sbo/theme/browser/javascripts/tabbar.js
+++ src/sbo.theme/sbo/theme/browser/javascripts/tabbar.js
@@ -1,1 +1,97 @@
-// tabbar
+$(document).ready(function()
+{
+    var animateTabbar = function(tabbar)
+    {
+        var time = 250;
+        
+        var selectedTab = tabbar.children("li.selected").get(0);
+        var activeTab = selectedTab;
+        var tabs = tabbar.children("li");
+        
+        tabs.removeClass("selected plain");
+        tabbar.append('<div class="marker"></div>')
+        
+        var marker = tabbar.children(".marker");
+        
+        marker.append('<div class="left-edge"></div>');
+        marker.append('<div class="right-edge"></div>');
+        
+        if (activeTab)
+        {
+            $(activeTab).addClass("active");
+            marker.css("left", activeTab.offsetLeft);
+            marker.css("width", activeTab.offsetWidth);
+            $(activeTab).children("ul").css("display", "block");
+        }
+        
+        var goToTab = function(target)
+        {
+            if (activeTab == target)
+            {
+                return;
+            }
+            
+            var sub = $(target).children("ul").get(0);
+            var oldSub = $(activeTab).children("ul").get(0);
+            
+            var slideOutDir = "up";
+            var slideInDir = "up";
+            
+            if (sub && oldSub)
+            {
+                var diff = tabs.index(target) - tabs.index(activeTab);
+                slideOutDir = diff > 0? "left": "right";
+                slideInDir = diff < 0? "left": "right";
+            }
+            
+            
+            $(oldSub).css("display", "none");
+            $(oldSub).hide("slide", {direction: slideOutDir}, time);
+            $(activeTab).removeClass("active");
+            
+            activeTab = target;
+            
+            if (activeTab)
+            {
+                marker.animate({
+                    left: activeTab.offsetLeft,
+                    width: activeTab.offsetWidth
+                }, time, "linear", function()
+                {
+                    if (target == activeTab)
+                    {
+                        $(target).addClass("active");
+                    }
+                });
+                $(sub).show("slide", {direction: slideInDir}, time);
+            }
+            else
+            {
+                marker.animate({
+                    left: 0,
+                    width: 0
+                }, time, "linear");
+            }
+        }
+        
+        tabs.mouseover(function(event)
+        {
+            goToTab(event.currentTarget);
+        });
+        
+        tabbar.mouseleave(function()
+        {
+            goToTab(selectedTab);
+        });
+    };
+    
+    var tabbar = $("#portal-globalnav");
+    
+    animateTabbar(tabbar);
+    
+    tabbar.find("ul.level-two").each(function(index, elem)
+    {
+        animateTabbar($(elem));
+    });
+     
+});
