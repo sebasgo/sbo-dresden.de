93fa23ce848eefea0263a67d4b1b62031a9bee3f
Sebastian Gottfried <sebastiangottfried@web.de>
2011-03-02 14:59:16 +0100

    sbo.theme: also add a custom news item view
    Works very much the way the concerts view does.

--- src/sbo.theme/sbo/theme/browser/views.py
+++ src/sbo.theme/sbo/theme/browser/views.py
@@ -21,3 +21,21 @@ class ConcertsView(BrowserView):
             sort_order='ascending'
         )
         return results;
+
+class NewsView(BrowserView):
+    
+    template = ViewPageTemplateFile('templates/news.pt')
+    
+    def __call__(self):
+        return self.template()
+    
+    def news(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        results = catalog(
+            portal_type='News Item',
+            path={'query': '/'.join(context.getPhysicalPath()), 'level': -1},
+            sort_on='created',
+            sort_order='descending'
+        )
+        return results;
