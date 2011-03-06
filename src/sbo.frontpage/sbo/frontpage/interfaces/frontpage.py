d26b72b1572a2153daf8a969d4e0ffbe0a528159
Sebastian Gottfried <sebastiangottfried@web.de>
2011-03-06 11:58:11 +0100

    sbo:frontpage: add content type boilerplate
    Automatically generated with paster.

---
+++ src/sbo.frontpage/sbo/frontpage/interfaces/frontpage.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+# -*- Additional Imports Here -*-
+
+
+class IFrontpage(Interface):
+    """A front page displaying latest news and upcoming concerts."""
+
+    # -*- schema definition goes here -*-
