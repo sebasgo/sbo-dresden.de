f553e43d55b26b1f48abfaa0181706d3cc4dc71c
Sebastian Gottfried <sebastiangottfried@web.de>
2011-03-08 12:26:50 +0100

    sbo.frontpage: markup for concerts
    Includes a methon to retrieve the next three concerts in view class and appropiate markup in the template.

--- src/sbo.frontpage/sbo/frontpage/browser/views.py
+++ src/sbo.frontpage/sbo/frontpage/browser/views.py
@@ -1,4 +1,5 @@
 from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -7,3 +8,19 @@ class FrontPageView(BrowserView):
 
     def __call__(self):
         return self.template()
+    
+    def concerts(self):
+        context = aq_inner(self.context)
+        limit = 3
+        src = context.concerts_folder
+        catalog = getToolByName(context, 'portal_catalog')
+        results = catalog(
+            portal_type='Event',
+            path={'query': '/'.join(src.getPhysicalPath()), 'level': -1},
+            start={'query': context.ZopeTime(), 'range': 'min'},
+            sort_on='start',
+            sort_order='ascending',
+            sort_limit=limit
+        )[:limit]
+        return [brain.getObject() for brain in results]
+        
