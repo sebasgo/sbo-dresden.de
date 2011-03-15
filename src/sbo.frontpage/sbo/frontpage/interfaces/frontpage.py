0d7688e0822a9bf1f724b07ce40b229c32c50c52
Sebastian Gottfried <sebastiangottfried@web.de>
2011-03-15 13:55:04 +0100

    sbo.frontpage: i18n and l10n

--- src/sbo.frontpage/sbo/frontpage/interfaces/frontpage.py
+++ src/sbo.frontpage/sbo/frontpage/interfaces/frontpage.py
@@ -10,21 +10,21 @@ class IFrontpage(Interface):
 
     cover_image = schema.Object(
         title=_(u"Cover image"),
-        description=_(u"The image displayed initially on the front page"),
+        description=_(u"The image displayed initially on the front page."),
         schema=IImageContent,
         required=True
     )
     
     news_folder = schema.Object(
         title=_(u"News folder"),
-        description=_(u"The folder containing the news items of the website"),
+        description=_(u"The folder containing the news items of the website."),
         schema=IBaseFolder,
         required=True
     )
     
     concerts_folder = schema.Object(
         title=_(u"Concerts folder"),
-        description=_(u"The folder containing the concerts of the website"),
+        description=_(u"The folder containing the concerts of the website."),
         schema=IBaseFolder,
         required=True
     )
