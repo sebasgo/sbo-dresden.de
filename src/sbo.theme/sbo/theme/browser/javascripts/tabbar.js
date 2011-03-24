c3095b799dc5d267799e7cd33572df159dd8d827
Sebastian Gottfried <sebastiangottfried@web.de>
2011-03-24 19:11:23 +0100

    sbo.theme: make animation of highlighted tab marker more fluid
    By stopping the old animation if there is still on running.

--- src/sbo.theme/sbo/theme/browser/javascripts/tabbar.js
+++ src/sbo.theme/sbo/theme/browser/javascripts/tabbar.js
@@ -4,101 +4,100 @@ jq(document).ready(function()
     {
         var time = 250;
 
-    var selectedTab = tabbar.children("li.selected").get(0);
-    var activeTab = selectedTab;
-    var tabs = tabbar.children("li");
-    var resetTimer = 0;
-
-    tabs.removeClass("selected plain");
-    tabbar.append('<div class="marker"></div>')
-
-    var marker = tabbar.children(".marker");
-
-    marker.append('<div class="left-edge"></div>');
-    marker.append('<div class="right-edge"></div>');
-
-    if (activeTab)
-    {
-        jq(activeTab).addClass("active");
-        marker.css("left", activeTab.offsetLeft);
-        marker.css("width", activeTab.offsetWidth);
-        jq(activeTab).children("ul").css("display", "block");
-    }
-
-    var goToTab = function(target)
-    {
-        if (activeTab == target)
+        var selectedTab = tabbar.children("li.selected").get(0);
+        var activeTab = selectedTab;
+        var tabs = tabbar.children("li");
+        var resetTimer = 0;
+    
+        tabs.removeClass("selected plain");
+        tabbar.append('<div class="marker"></div>')
+    
+        var marker = tabbar.children(".marker");
+    
+        marker.append('<div class="left-edge"></div>');
+        marker.append('<div class="right-edge"></div>');
+    
+        if (activeTab)
         {
-            return;
+            jq(activeTab).addClass("active");
+            marker.css("left", activeTab.offsetLeft);
+            marker.css("width", activeTab.offsetWidth);
+            jq(activeTab).children("ul").css("display", "block");
+        }
+    
+        var goToTab = function(target)
+        {
+            if (activeTab == target)
+            {
+                return;
+            }
+    
+            var sub = jq(target).children("ul").get(0);
+            var oldSub = jq(activeTab).children("ul").get(0);
+        
+            var slideOutDir = "up";
+            var slideInDir = "up";
+        
+            if (sub && oldSub)
+            {
+                var diff = tabs.index(target) - tabs.index(activeTab);
+                slideOutDir = diff > 0? "left": "right";
+                slideInDir = diff < 0? "left": "right";
+            }
+            
+            jq(oldSub).css("display", "none");
+            jq(oldSub).hide("slide", {direction: slideOutDir}, time);
+            jq(activeTab).removeClass("active");
+        
+            activeTab = target;
+        
+            if (activeTab)
+            {
+                marker.stop().animate({
+                    left: activeTab.offsetLeft,
+                    width: activeTab.offsetWidth
+                }, time, "linear", function()
+                {
+                    if (target == activeTab)
+                    {
+                        jq(target).addClass("active");
+                    }
+                });
+                jq(sub).show("slide", {direction: slideInDir}, time, function()
+                {
+                    if (target != activeTab)
+                    {
+                        jq(sub).css("display", "");
+                    }
+                });
+            }
+            else
+            {
+                marker.animate({
+                    left: 0,
+                    width: 0
+                }, time, "linear");
+            }
         }
 
-    var sub = jq(target).children("ul").get(0);
-    var oldSub = jq(activeTab).children("ul").get(0);
-
-    var slideOutDir = "up";
-    var slideInDir = "up";
-
-    if (sub && oldSub)
-    {
-        var diff = tabs.index(target) - tabs.index(activeTab);
-        slideOutDir = diff > 0? "left": "right";
-        slideInDir = diff < 0? "left": "right";
-    }
-
-
-    jq(oldSub).css("display", "none");
-    jq(oldSub).hide("slide", {direction: slideOutDir}, time);
-    jq(activeTab).removeClass("active");
-
-    activeTab = target;
-
-    if (activeTab)
-    {
-        marker.animate({
-            left: activeTab.offsetLeft,
-            width: activeTab.offsetWidth
-        }, time, "linear", function()
+        tabs.children("a").mouseover(function(event)
         {
-            if (target == activeTab)
-            {
-                jq(target).addClass("active");
-            }
+            goToTab(event.currentTarget.parentNode);
         });
-        jq(sub).show("slide", {direction: slideInDir}, time, function()
+    
+        tabbar.mouseleave(function()
         {
-            if (target != activeTab)
+            resetTimer = setTimeout(function()
             {
-                jq(sub).css("display", "");
-            }
+                goToTab(selectedTab);
+            }, 1000);
         });
-    }
-    else
-    {
-        marker.animate({
-            left: 0,
-            width: 0
-        }, time, "linear");
-    }
-}
-
-    tabs.children("a").mouseover(function(event)
-    {
-        goToTab(event.currentTarget.parentNode);
-    });
-
-    tabbar.mouseleave(function()
-    {
-        resetTimer = setTimeout(function()
+    
+        tabbar.mouseenter(function()
         {
-            goToTab(selectedTab);
-        }, 1000);
-    });
-
-    tabbar.mouseenter(function()
-    {
-        clearTimeout(resetTimer);
-    });
-};
+            clearTimeout(resetTimer);
+        });
+    };
 
     if (jq.browser.msie)
     {
