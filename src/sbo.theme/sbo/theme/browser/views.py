68801d4e005e97131b6ad2f9eda641e114373492
Sebastian Gottfried <sebastiangottfried@web.de>
2011-07-27 15:35:53 +0200

    sbo.theme: integrate quick uploader into gallery
    The quick uploader portlet breaks the gallery upload. Therefore we integrate the quick uploader directely in the gallery template under the actual content.
    
    Code in the view is copied & pasted for the most part from the quick uploader portlet.

--- src/sbo.theme/sbo/theme/browser/views.py
+++ src/sbo.theme/sbo/theme/browser/views.py
@@ -1,7 +1,13 @@
-from Acquisition import aq_inner
+from Acquisition import aq_inner, aq_base, aq_parent
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.FactoryTool import TempFolder
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+def isTemporary(obj):
+    """Check to see if an object is temporary"""
+    parent = aq_base(aq_parent(aq_inner(obj)))
+    return hasattr(parent, 'meta_type') and parent.meta_type == TempFolder.meta_type
 
 class ConcertsView(BrowserView):
     
@@ -44,6 +50,12 @@ class GalleryView(BrowserView):
     
     template = ViewPageTemplateFile('templates/gallery.pt')
     
+    def __init__(self, context, request):
+        BrowserView.__init__(self, context, request)
+        context = aq_inner(self.context)
+        self.ploneview = context.restrictedTraverse('@@plone')
+        self.pm = getToolByName(context, 'portal_membership')
+        
     def __call__(self):
         return self.template()
 
@@ -58,4 +70,24 @@ class GalleryView(BrowserView):
             batch=(limit is not None),
             b_size=limit
         )
+
+    def is_uploader_available(self):
+        context = aq_inner(self.context)
+        if self.ploneview.isStructuralFolder() \
+           and self.pm.checkPermission('Add portal content', context) \
+           and not isTemporary(context):
+            return True
+        return False
+    
+    def get_upload_url(self):
+        """
+        return upload url
+        in current folder
+        """
+        context = aq_inner(self.context)
+        folder_url = self.ploneview.getCurrentFolderUrl()                      
+        return '%s/@@quick_upload' %folder_url
         
+    def get_data_for_upload_url(self):
+        data_url = 'mediaupload=%s' % 'image' 
+        return data_url      
