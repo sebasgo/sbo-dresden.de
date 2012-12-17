137053c75f223da2de4ea81099cca1107643641e
Sebastian Gottfried <sebastiangottfried@web.de>
2012-12-17 20:09:41 +0100

    sbo.inkstain: send notification mail on new guestbook entry

--- src/sbo.inkstain/sbo/inkstain/browser/writeentry.py
+++ src/sbo.inkstain/sbo/inkstain/browser/writeentry.py
@@ -7,8 +7,10 @@ from zope.interface import Interface
 from zope import schema
 from zope.component import getMultiAdapter
 from z3c.form import form, field, button
+from ZODB.POSException import ConflictError
 from plone.z3cform.layout import FormWrapper, wrap_form
 from plone.formwidget.recaptcha.widget import ReCaptchaFieldWidget
+from Products.CMFCore.utils import getToolByName
 from Products.statusmessages.interfaces import IStatusMessage
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -124,6 +126,8 @@ class BaseForm(form.Form):
 
             context.addEntry(entry)
 
+            self.send_notification_mail(entry)
+
             confirm = _(u"Thank you for your guestbook entry!")
             IStatusMessage(self.request).addStatusMessage(confirm, type='info')
 
@@ -131,6 +135,35 @@ class BaseForm(form.Form):
             return ''
 
         return ''
+
+    def send_notification_mail(self, entry):
+        context = aq_inner(self.context)
+        urltool = getToolByName(context, 'portal_url')
+        plone_utils = getToolByName(context, 'plone_utils')
+        portal = urltool.getPortalObject()
+        send_to_address = portal.getProperty('email_from_address')
+        envelope_from = portal.getProperty('email_from_address')
+        subject = _(u"New guestbook entry")
+        encoding = portal.getProperty('email_charset')
+        message_tpl = _(u"Name: {name}\nEmail: {mail}\nHomepage: {homepage}\n\n{text}")
+        message = message_tpl.format(
+            name=entry.name,
+            mail=entry.email_address,
+            homepage=entry.homepage_address,
+            text=entry.message
+        ).encode(encoding)
+	try:
+            host = getToolByName(self, 'MailHost')
+            result = host.send(message, send_to_address, envelope_from,
+                                   subject=subject, charset=encoding)
+        except ConflictError:
+            raise
+        except: # TODO Too many things could possibly go wrong. So we catch all.
+            exception = plone_utils.exceptionString()
+            message = _(u'Unable to send mail: ${exception}',
+                            mapping={u'exception' : exception})
+            plone_utils.addPortalMessage(message, 'error')
+
 
 class InlineWriteEntryForm(FormWrapper):
      """ Form view which renders z3c.forms embedded in a portlet.
