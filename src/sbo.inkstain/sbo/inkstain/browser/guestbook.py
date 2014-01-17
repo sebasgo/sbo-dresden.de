8b0dd05cbbe71cb805a58e8107ad2920e6233503
Sebastian Gottfried <sebastiangottfried@web.de>
2014-01-17 10:31:24 +0100

    sbo.inkstain: update view to match new content types
    Including controls to mark entries as spam or publish them.

--- src/sbo.inkstain/sbo/inkstain/browser/guestbook.py
+++ src/sbo.inkstain/sbo/inkstain/browser/guestbook.py
@@ -1,11 +1,10 @@
 from Acquisition import aq_inner
 from zope.security import checkPermission
 
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
-
-from plone.memoize.view import memoize
 
 from sbo.inkstain.browser.writeentry import InlineWriteEntryForm
 from sbo.inkstain import InkstainMessageFactory as _
@@ -15,17 +14,29 @@ class GuestbookView(BrowserView):
     template = ViewPageTemplateFile('guestbook.pt')
 
     def __call__(self):
+        context = aq_inner(self.context)
         postback = True
         form = self.request.form
-
-        deleteButton = form.get('form.button.Delete', False)
+        message_ids = form.get('messages', [])
 
         if form.get('form.button.Delete', False):
-            context = aq_inner(self.context)
-            deleteIds = form.get('messages', [])
-            context.manage_delObjects(ids = deleteIds)
+            context.manage_delObjects(ids=message_ids)
             confirm = _(u"The marked entries have been deleted.")
             IStatusMessage(self.request).addStatusMessage(confirm, type='info')
+            postback = False
+
+        if form.get('form.button.Publish', False):
+            for id in message_ids:
+                if context.hasObject(id):
+                    message = context[id]
+                    message.moderation_state = 'published'
+            postback = False
+
+        if form.get('form.button.Spam', False):
+            for id in message_ids:
+                if context.hasObject(id):
+                    message = context[id]
+                    message.moderation_state = 'spam'
             postback = False
 
         if postback:
@@ -37,15 +48,28 @@ class GuestbookView(BrowserView):
     def createWriteEntryForm(self):
         context = aq_inner(self.context)
         view = InlineWriteEntryForm(context, self.request)
-        view = view.__of__(context) 
+        view = view.__of__(context)
         return view();
 
-    @memoize
     def entries(self):
         context = aq_inner(self.context)
-        entries = context.objectValues()
-        entries.sort(key=lambda entry: entry.date, reverse=True)
-        return entries
+        catalog = getToolByName(context, 'portal_catalog')
+        if self.canReviewGuestbook():
+            results = catalog(
+                portal_type='sbo.inkstain.guestbookentry',
+                path={'query': '/'.join(context.getPhysicalPath()), 'level': -1},
+                sort_on='entry_date',
+                sort_order='descending'
+            )
+        else:
+            results = catalog(
+                portal_type='sbo.inkstain.guestbookentry',
+                path={'query': '/'.join(context.getPhysicalPath()), 'level': -1},
+                moderation_state='published',
+                sort_on='entry_date',
+                sort_order='descending'
+            )
+        return results
 
     def canAddGuestbookEntries(self):
         return checkPermission("sbo.inkstain.AddGuestbookEntry", self.context)
